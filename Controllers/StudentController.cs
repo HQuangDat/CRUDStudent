6a3b868658401fa68592aa8ac769e8ca83beae66
RidGud <hoquangdat123@gmail.com>
2024-07-17 12:05:10 +0700

    Finish the Create in CRUD

--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -1,12 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CRUDStudent.Data;
+using CRUDStudent.Models.Entity;
+using Microsoft.AspNetCore.Mvc;
+using MVCFirst.Controllers;
 
 namespace CRUDStudent.Controllers
 {
     public class StudentController : Controller
     {
+        private readonly ApplicationDbContext dbContext;
+
+        public StudentController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
         [HttpGet]
         public IActionResult Add()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(Student student)
+        {
+            var student_add = new Student
+            {
+                Name = student.Name,
+                Major = student.Major,
+                dob = student.dob
+            };
+            await dbContext.Students.AddAsync(student_add);
+            await dbContext.SaveChangesAsync();
             return View();
         }
     }
